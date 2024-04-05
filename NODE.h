ad797c34d6555e1dbacb35fbdb04508357be08d8
ThammanatLerdwijitjarud <156405576+ThammanatLerdwijitjarud@users.noreply.github.com>
2024-04-05 10:30:07 +0000

    class animal

--- NODE.h
+++ NODE.h
@@ -6,47 +6,58 @@ class Animal {
     private:
         string name, color;
         char gender;
-        int age;
+        int age, weight;
     public:
         // string find_color(string); //use sorting
         // string set_color(string);
         // string get_color();
-        void set_name(string);
+        Animal(string = "noname", char = 'F', int = 1, int = 5);
         string get_name();
-        void set_gender(char);
         char get_gender();
-        void set_age(int);
         int get_age();
-
+        void display();
 };
 
 class cat : public Animal{
+private :
+    int no_cat=0;
 public:
-    cat() {
-        cout<<"Create Cat"<<endl;
-    }
+    cat(string,char,int,int);
 };
 
 class dog : public Animal{
+private:
+    int no_dog=0;
 public:
-    dog() {
-        cout<<"Create Dog"<<endl;
-    }
+    dog(string,char,int,int);
 };
 
-void Animal::set_name(string n)
+Animal::Animal(string n, char g, int a, int w)
 {
     name = n;
+    gender = g;
+    age = a;
+    cout<<"Create animal"<<endl;
 }
+
+
+cat::cat(string n,char g, int a ,int w) : Animal(n,g,a,w)
+{
+    no_cat++;
+    cout<<"Number of cat = "<<no_cat<<endl;
+}
+
+
+dog::dog(string n,char g, int a ,int w) : Animal(n,g,a,w)
+{
+    no_dog++;
+    cout<<"Number of dog = "<<no_dog<<endl;
+}
+
 
 string Animal::get_name()
 {
     return name;
-}
-
-void Animal::set_gender(char g)
-{
-    gender = g;
 }
 
 char Animal::get_gender()
@@ -54,13 +65,16 @@ char Animal::get_gender()
     return gender;
 }
 
-void Animal::set_age(int a)
-{
-    age = a;
-}
-
 int Animal::get_age()
 {
     return age;
 }
+
+void Animal::display()
+{
+    cout<<"Name : "<<get_name()<<endl;
+    cout<<"Gender : "<<get_gender()<<endl;
+    cout<<"Age : "<<get_age()<<" years old"<<endl;
+}
+
 
--- main.cpp
+++ main.cpp
@@ -5,12 +5,6 @@
 using namespace std;
 int main()
 {
-    cat cat1;
-    cat1.set_name("Garfield");
-    cat1.set_gender('F');
-    cat1.set_age(1);
-
-    cout<<"Name : "<<cat1.get_name()<<endl;
-    cout<<"Gender : "<<cat1.get_gender()<<endl;
-    cout<<"Age : "<<cat1.get_age()<<" years old"<<endl;
+    cat cat1("Garfield",'F',1,2);
+    cat1.display();
 }
