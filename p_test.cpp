f46867ed7dabd1d8ee1fa7c74019760346bb5ccd
ThammanatLerdwijitjarud <156405576+ThammanatLerdwijitjarud@users.noreply.github.com>
2024-04-19 08:15:39 +0000

    mix

--- p_test.cpp
+++ p_test.cpp
@@ -7,6 +7,33 @@
 
 using namespace std;
 
+void donate(){
+    donation donation;
+
+    char choice;
+    do{
+        string name;
+        double amount;
+
+        cout << "Enter donor's name: ";
+        cin >> name;
+
+        cout << "Enter donation amount: ";
+        cin >> amount;
+
+        donation.adddonation(Donor(name, amount));
+
+        cout << "Do you want to add another donation? (y/n): ";
+        cin >> choice;
+    } while (choice == 'y' || choice == 'Y');
+
+    donation.display();
+
+
+    cout << "Total Donations: ฿" << donation.total() << endl;
+
+}
+
 
 int main () {
     int i;
