6b4d5a8ef4e08d5954fd0f61e9a1d11604cce42b
glittereiei <167741246+glittereiei@users.noreply.github.com>
2024-04-25 08:00:26 +0000

    add month

--- donation.cpp
+++ donation.cpp
@@ -81,6 +81,10 @@ bool validateDate(const string& date) {
     {
         return false;
     }
+    else if((month==4 || month==6 || month==9 || month==11) && (day<1 || day>30)){
+        return false;
+    }
+
     else if (day < 1 || day > 31 || month < 1 || month > 12) {
         return false;
     }
